d71b91d5bf18c488b85afd219b1b1862fa9caef6
Zeidha <45020917+Zeidha@users.noreply.github.com>
2021-11-30 20:54:39 -0500

    Update data.jsx

--- src/Home/data.jsx
+++ src/Home/data.jsx
@@ -46,7 +46,7 @@ export const team = [
     title: 'Zeid Habbab',
     content: [
       <p key="1">Executive Director</p>,
-      <p key="2">Zeid originally hails from Georgia, USA but grew up splitting his time between Damascus, Syria and Michigan, USA. The idea for Lazzzy originated with Zeid, stemming from his own desire for better note-taking tools to decode and organize the overload of online academic resources. His favorite hobbies are reading and video games where he currently maintains a solid 1.3 KDA on Call of Duty. When asked how does he <i>embrace the lazzzy Zeid</i> he replied, “when I get a food coma in the middle of a meal, I take a nap to finish my food later.” </p>,
+      <p key="2">Zeid was born in Georgia, USA but grew up splitting his time between Damascus, Syria and Michigan, USA. The idea for Lazzzy originated with Zeid, stemming from his own desire for better note-taking tools to decode and organize the overload of online academic resources. His favorite hobbies are reading and video games where he currently maintains a solid 1.3 KDA on Call of Duty. When asked how does he <i>embrace the lazzzy Zeid</i> he replied, “when I get a food coma in the middle of a meal, I take a nap to finish my food later.” </p>,
     ],
     person: 'https://lazzzy.app/static/zeid.png',
   },
@@ -54,7 +54,7 @@ export const team = [
     title: 'Ari Brown',
     content: [
       <p key="1">Operations Director</p>,
-      <p key="2">Ari originally hails from New Jersey, USA but grew up so close to NYC that you are able to see the city skyline from his childhood home. Ari is a sports buff and proudly admits to the misery that comes with being a New York Mets fan. He is an ultimate frisbee aficionado and currently plays for the UMich club team. His favorite hobby is cooking and currently lives with four others who cannot, so he gets to experiment with new recipes. Food is what led Ari to his last position at an Italian food manufacturer & distributor, Mama Mancini’s, where he was able to work and learn directly from their COO in logistics and inventory management. When asked how does he <i>embrace the lazzzy Ari</i> he replied, “I embrace the lazy me by knowing how much to do in as little time as possible.”</p>,
+      <p key="2">Ari was raised New Jersey, USA and grew up so close to NYC that you are able to see the city skyline from his childhood home. Ari is a sports buff and proudly admits to the misery that comes with being a New York Mets fan. He is an ultimate frisbee aficionado and currently plays for the UMich club team. His favorite hobby is cooking and currently lives with four others who cannot, so he gets to experiment with new recipes. Food is what led Ari to his last position at an Italian food manufacturer & distributor, Mama Mancini’s, where he was able to work and learn directly from their COO in logistics and inventory management. When asked how does he <i>embrace the lazzzy Ari</i> he replied, “I embrace the lazy me by knowing how much to do in as little time as possible.”</p>,
     ],
     person: 'https://lazzzy.app/static/ari.png',
 
@@ -63,7 +63,7 @@ export const team = [
     title: 'Julius Welschof',
     content: [
       <p key="1">Marketing Director</p>,
-      <p key="2">Julius originally hails from Bavaria, Germany where he completed his mechanical engineering apprenticeship for Kathrein from 2013 to 2016 and worked as a Machine Engineer at Krones until 2018 before coming to the United States in July 2018. His favorite hobbies outside of smashing through offensive lines and sacking quarterbacks on Saturday's are golfing, playing video games, and skiing. When asked how does he <i>embrace the lazzzy Julius</i> he replied, “What lazy means to me is instead of what you typically think, it's that I'd rather work smarter not harder. That's what being lazy means to me. More free time to myself.  With Lazzzy I am going to save a lot of time I can finally use to focus on my other hobbies I don't have time for as a student-athlete.”</p>,
+      <p key="2">Julius is a native German, growing up in Bavaria where he completed his mechanical engineering apprenticeship for Kathrein from 2013 to 2016 and worked as a Machine Engineer at Krones until 2018 before coming to the United States in July 2018. His favorite hobbies outside of smashing through offensive lines and sacking quarterbacks on Saturday's are golfing, playing video games, and skiing. When asked how does he <i>embrace the lazzzy Julius</i> he replied, “What lazy means to me is instead of what you typically think, it's that I'd rather work smarter not harder. That's what being lazy means to me. More free time to myself.  With Lazzzy I am going to save a lot of time I can finally use to focus on my other hobbies I don't have time for as a student-athlete.”</p>,
     ],
     person: 'https://lazzzy.app/static/julius.png',
 
